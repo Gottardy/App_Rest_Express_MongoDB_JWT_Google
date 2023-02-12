abe26f6edf3c62e99c324e8c4bd0a1d8211a0ba6
Gottardy <gottgab@gmail.com>
2023-02-11 19:28:48 -0500

    se crea el metodo de busqueda general para usuarios

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,15 +1,87 @@
 const { request, response } = require('express');
+const mongoose = require('mongoose');
+
+const Usuario = require('../models/usuario')
+const Categoria = require('../models/categoria')
+const Producto = require('../models/producto')
+const Role = require('../models/role');
+const usuario = require('../models/usuario');
+
+const coleccionesPermitidas =[
+    'usuarios',
+    'categoria',
+    'productos',
+    'roles'
+];
+
+const buscarUsuarios = async(termino = '', res = response)=>{
+    const esMongoID = mongoose.Types.ObjectId.isValid(termino);
+    
+    if (esMongoID) {
+        const usuario = await Usuario.findById(termino);
+        return res.json({
+            // Enviamos losresultados encontrados utilizando la validacion ternaria
+           results: (usuario) ? [usuario]:[],
+        });
+    }
+    // Nos apoyamos con la funcionalidad de la libreria RegExp para crear una expresion regular insensible a mayus/minus
+    const expregTermino = new RegExp(termino, 'i');
+    const usuarios = await Usuario.find({
+        $or:[
+            {nombre:expregTermino},{correo:expregTermino}
+        ],
+        $and:[
+            {estado: true}
+        ]
+    });
+    const totalRegistrosConsultados =  Object.keys(usuarios).length;
+    res.json({
+        totalRegistrosConsultados,
+        results: usuarios,
+    });
+    
+    
+}
 
 const buscar = async (req = request, res = response) => {
     console.log('GET sended Busqueda All');
-
     const {coleccion, termino}=req.params;
-
-    res.json({
-        msg:'Todo ok',
-        coleccion,
-        termino
-    })
+    if(!coleccionesPermitidas.includes(coleccion)){
+        return res.status(400).json({
+           msg:`Las colecciones permitidas ${coleccionesPermitidas}`
+        });
+    }
+    switch (coleccion) {
+        case 'categorias':
+            console.log(termino)
+            res.json({
+                ok:'todo ok',
+            });
+            break;
+        case 'productos':
+            console.log(termino)
+            res.json({
+                ok:'todo ok',
+            });
+            break;
+        case 'usuarios':
+            // console.log(termino)
+            // res.json({
+            //     ok:'todo ok',
+            // });
+            buscarUsuarios(termino, res);
+            break;
+        case 'roles':
+            console.log(termino)
+            res.json({
+                ok:'todo ok',
+            });
+            break;
+        default:
+            res.status(500).json({
+                msg:`Busqueda no permitida para ${coleccion}`
+            })
+    }
 }
 
 module.exports = { buscar }
