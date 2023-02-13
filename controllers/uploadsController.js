8f4660be9aebda7f4ed233c263dee2e722103822
Gottardy <gottgab@gmail.com>
2023-02-12 19:18:21 -0500

    se valida la extension permitida del archivo

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -4,19 +4,29 @@ const { request, response } = require('express');
 const cargarArchivo = async (req = request, res = response) =>{
 
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-    return res.status(400).send('No se cargaron archivos.');
+    return res.status(400).json({msg:'No se cargaron archivos.'});
   }
 
   const {archivo} = req.files;
+// Separamos el nombre del archivo con su extension
+  const archivoCortado = archivo.name.split('.')
+// Obtenemos la extension de la ultima posicion del nombre cortado
+  const extension = archivoCortado[archivoCortado.length - 1];
+// Las extensiones que permitiremos subir
+  const extensionValidas = ['png','jpg','jpeg','gif','txt','md'];
+  if (!extensionValidas.includes(extension)) {
+    return res.status(400).json({msg:'Extension no es permitida'});
+  }
+
   const uploadPath = path.join(__dirname,'../uploads/',archivo.name);
 
   // Use el método mv() para colocar el archivo en algún lugar de su servidor
-  archivo.mv(uploadPath, (err) =>{
-    if (err)
-      return res.status(500).json({err});
+//   archivo.mv(uploadPath, (err) =>{
+//     if (err)
+//       return res.status(500).json({err});
 
-    res.json({msg:'File uploaded! =>'+uploadPath});
-  });
+//     res.json({msg:'File uploaded! =>'+uploadPath});
+//   });
 }
 
 module.exports ={
