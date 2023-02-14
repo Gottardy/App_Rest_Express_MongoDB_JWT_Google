95a0189f09a2521b3ad817e3d0cafd9365447e90
Gottardy <gottgab@gmail.com>
2023-02-13 21:27:29 -0500

    se agrega validaciones a la ruta y formatos permitidos

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -6,11 +6,19 @@ const cargarArchivo = async (req = request, res = response) =>{
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     return res.status(400).json({msg:'No se cargaron archivos.'});
   }
-  // Subir Archivos formato Imagenes ['png', 'jpg', 'jpeg', 'gif']
-  const rutaCompleta = await subirArchivo(req.files);
-  res.json({
-    ubicacion:rutaCompleta
-  });
+  
+  try {
+    // Subir Archivos formato Imagenes ['png', 'jpg', 'jpeg', 'gif'] pero se le puede agregar otros tipos para diferentes archivos
+    const rutaCompleta = await subirArchivo(req.files, ['png', 'jpg', 'jpeg', 'gif', 'txt', 'md'], 'detodo');
+    res.json({
+      ubicacion: rutaCompleta
+    });
+  } catch (error) {
+    res.status(400).json({
+      error
+    });
+  }
+
 
 }
 
