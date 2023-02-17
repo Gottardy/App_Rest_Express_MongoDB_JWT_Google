2c180694ad4e204fc51840504b8a824aa9324f18
Gottardy <gottgab@gmail.com>
2023-02-16 19:36:11 -0500

    actualizacion de las rutas y la validacion de los parametros recibidos

--- routes/uploadsRoute.js
+++ routes/uploadsRoute.js
@@ -1,11 +1,20 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
-const { cargarArchivo } = require('../controllers/uploadsController');
+const { cargarArchivo, actualizarImagen } = require('../controllers/uploadsController');
+const { coleccionesPermitidas } = require('../DB/validators.db');
 
 const { validarParametros } = require('../middlewares/validarCampos');
+const {validarArchivoSubir} = require('../middlewares/validarArchivo')
 
 const router = Router();
 
-router.post('/',[], cargarArchivo);
+router.post('/',[validarArchivoSubir], cargarArchivo);
+
+router.put('/:coleccion/:id',[
+    validarArchivoSubir,
+    check('id','No es un ID valido').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas(c,['usuarios','productos']) ),
+    validarParametros
+], actualizarImagen);
 
 module.exports = router;
