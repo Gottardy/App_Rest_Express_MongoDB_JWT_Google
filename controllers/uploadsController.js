0e16f95336d0aa71875db7940f1607b62ef35266
Gottardy <gottgab@gmail.com>
2023-02-16 19:34:31 -0500

    nuevo metodo de actualizar la imagen a laas colecciones de productos y usuarios

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,11 +1,13 @@
 const { request, response } = require('express');
 const {subirArchivo} = require('../helpers/subir-archivo');
+const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
 
 const cargarArchivo = async (req = request, res = response) =>{
 
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-    return res.status(400).json({msg:'No se cargaron archivos.'});
-  }
+  // if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+  //   return res.status(400).json({msg:'No se cargaron archivos.'});
+  // }
   
   try {
     // Subir Archivos formato Imagenes ['png', 'jpg', 'jpeg', 'gif'] pero se le puede agregar otros tipos para diferentes archivos
@@ -22,6 +24,46 @@ const cargarArchivo = async (req = request, res = response) =>{
 
 }
 
+const actualizarImagen = async (req = request, res = response) =>{
+  // if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+  //   return res.status(400).json({msg:'No se cargaron archivos.'});
+  // }  
+  const {id, coleccion} = req.params;
+  let modelo;
+  switch (coleccion) {
+    case 'usuarios':
+      modelo = await Usuario.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg:`No existe un usuario con el id :{${id}}`
+        });
+      }
+
+      break;
+    case 'productos':
+      modelo = await Producto.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg:`No existe un producto con el id :{${id}}`
+        });
+      }
+      break;
+  
+    default:
+      return res.status(500).json({
+        msg:'Validacion de coleccion erronea'
+      })
+  }
+
+  modelo.img = await subirArchivo(req.files, ['png', 'jpg', 'jpeg', 'gif', 'svg'], coleccion);
+   await modelo.save();
+
+  res.json({
+    modelo
+  });
+}
+
 module.exports ={
-    cargarArchivo
+    cargarArchivo,
+    actualizarImagen
 }
